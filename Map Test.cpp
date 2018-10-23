db3bf250ecead4f51286ea8c37735f5dc0df4143
karlosdb <44413652+karlosdb@users.noreply.github.com>
2018-10-23 16:52:40 -0400

    Update Map Test.cpp

--- Map Test.cpp
+++ Map Test.cpp
@@ -1,19 +1,52 @@
 #include "MapHeader.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
 	Map map(4, 4);
 
+	cout << boolalpha << map.getEnemyState(0, 0) << endl
+		<< map.getItemState(0, 0) << endl
+		<< map.getTerrain(0, 0) << endl
+		<< map.getLength() << endl
+		<< map.getWidth() << endl << endl;
 
 	map.setDimensions(2, 2);
 
+	cout <<  boolalpha << map.getEnemyState(0, 0) << endl
+		<< map.getItemState(0, 0) << endl
+		<< map.getTerrain(0, 0) << endl
+		<< map.getLength() << endl
+		<< map.getWidth() << endl << endl;
+
+
 	map.setTerrain(0, 0, Map::Terrain::WATER);
 
-	map.switchEnemy(0, 1);
+	cout << map.getEnemyState(0, 0) << endl
+		<< map.getItemState(0, 0) << endl
+		<< map.getTerrain(0, 0) << endl
+		<< map.getLength() << endl
+		<< map.getWidth() << endl << endl;
 
-	map.switchItem(1, 1);
-	cout << "hi";
+	map.switchEnemy(0, 0);
+
+	cout << map.getEnemyState(0, 0) << endl
+		<< map.getItemState(0, 0) << endl
+		<< map.getTerrain(0, 0) << endl
+		<< map.getLength() << endl
+		<< map.getWidth() << endl << endl;
+
+	map.switchItem(0, 0);
+
+	cout << map.getEnemyState(0, 0) << endl
+		<< map.getItemState(0, 0) << endl
+		<< map.getTerrain(0, 0) << endl
+		<< map.getLength() << endl
+		<< map.getWidth() << endl << endl;
+
+
+	cin.ignore();
 	return 0;
 }
