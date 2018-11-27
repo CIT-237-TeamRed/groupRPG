8566f1ba66b8ca7e3541d9fc88ca1e3260c10a54
karlosdb <44413652+karlosdb@users.noreply.github.com>
2018-11-26 22:50:01 -0500

    Created "FULLTEST"
    Created a file intended to be the main, where everything gets implemented

---
+++ groupRPG/FULLTEST.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "Map.h"
+
+
+#include "Hero.h"   // Need this?
+#include "Mage.h"
+#include "Warrior.h"
+#include "Leader.h"
+
+#include "Item.h"
+#include "Ingestible.h"
+#include "Physical.h"
+#include "Tool.h"
+#include "Weapon.h"
+#include "Magic.h"
+
+#include "Enemy.h" // Do we need this?
+#include "Benign.h"
+#include "Thief.h"
+#include "Silverman.h"
+#include "vicious.h"
+#include "Banshee.h"
+#include "Bigfoot.h"
+
+
+
+
+#include <iostream>
+
+int main() {
+	
+	/*
+	Intro
+		Title
+		Load File
+		New Game
+		Instructions
+		Quit option
+	Explore
+		describe surroundings
+		display stats
+		obtain items
+		edit inventory
+		encounter enemies
+		exit/quit/save option //exit saves first
+	Combat
+		edit inventory
+		use items
+		attack (turn based)
+	Game Over
+		death screen
+		>should bring you back to intro, where you can load save, or quit
+		
+	*/
+
+	return 0;
+}
